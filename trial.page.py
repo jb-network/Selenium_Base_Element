0d860013b53608d905aaa0af8e09b2b831575f97
jb-network <85036487+jb-network@users.noreply.github.com>
2022-10-31 12:17:19 -0500

    Create trial.page.py

---
+++ trial.page.py
@@ -0,0 +1,30 @@
+from selenium.webdriver.common.by import By
+from base_element import BaseElement
+from base_page import BasePage
+from locator import Locator
+
+
+class TrialPage(BasePage):
+    # Everything has to be unique to this page, describing this page.  Everything else should be in the base page
+    url = 'https://techstepacademy.com/trial-of-the-stones'
+
+    @property
+    def stone_input(self):
+        # input# is short hand for ID
+        locator = Locator(By.CSS_SELECTOR, 'input#r1Input')
+        return BaseElement(self.driver, locator=locator)
+
+    @property
+    def stone_button(self):
+        locator = Locator(By.CSS_SELECTOR, 'button#r1Btn')
+        return BaseElement(self.driver, locator=locator)
+
+    @property
+    def secrets_input(self):
+        locator = Locator(By.CSS_SELECTOR, 'input#r2Input')
+        return BaseElement(self.driver, locator=locator)
+
+    @property
+    def secrets_button(self):
+        locator = Locator(By.CSS_SELECTOR, 'button#r2Butn')
+        return BaseElement(self.driver, locator=locator)
