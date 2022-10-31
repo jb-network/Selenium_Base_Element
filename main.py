cba0fb9a9e5b3e4fe2b5cea353ce538459f8cde5
jb-network <85036487+jb-network@users.noreply.github.com>
2022-10-31 12:13:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from selenium import webdriver
-
-from Python_Daily_Work.Traning_Ground import TrainingGroundPage
+from traning_ground import TrainingGroundPage
+from trial_page import TrialPage
 
 # Test Setup
 browser = webdriver.Chrome()
-test_value = "it worked"
 
-# Test
+# Trial_Page
+trial_page = TrialPage(driver=browser)
+trial_page.go()
+trial_page.stone_input.input_text("rock")
+trial_page.stone_button.click()
+# Just for this test, this should not be in a real test
+input()
+
+# Training Grounds
 trng_page = TrainingGroundPage(driver=browser)
 trng_page.go()
 # used @property to remove button1().click()
-trng_page.button1.click()
+assert trng_page.button1.text() == "Button1", "Unexpected button1 text"
+print("Passed")
+input()
+browser.quit()
+# trng_page.button1.click()
